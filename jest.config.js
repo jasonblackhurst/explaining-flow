45ccc917af9a43790fa8159c5684c924e00fb7e9
Slim <jasonblackhurst@gmail.com>
2025-06-21 17:46:46 -0400

    Fix test configuration conflicts

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/spec/**/*.spec.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!src/**/*.spec.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html']
+}; 
